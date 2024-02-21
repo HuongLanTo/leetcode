f41eea32ef4d423e3bf1125aa8f222b11f29fd40
HuongLanTo <tohuonglan1705@gmail.com>
2024-02-20 23:08:14 -0600

    242. Valid Anagram

---
+++ javascript/valid-anagram.js
@@ -0,0 +1,13 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    let sArr = s.split("").sort();
+    let tArr = t.split("").sort();
+    for (let i = 0; i < sArr.length; i++) {
+        if (sArr[i] !== tArr[i]) {
+            return false;
+        }
+    }
+    return true;
+};
