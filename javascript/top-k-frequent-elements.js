6a162c0e0760ec13b196c2b20e5e029e5e7ff6d3
HuongLanTo <tohuonglan1705@gmail.com>
2024-03-23 12:38:06 -0500

    347. Top K Frequent Elements

---
+++ javascript/top-k-frequent-elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            map.set(nums[i], map.get(nums[i]) + 1);
+        } else {
+            map.set(nums[i], 1);
+        }
+    }
+    let sorted = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
+    return sorted.slice(0, k).map(entry => entry[0]);
+};
