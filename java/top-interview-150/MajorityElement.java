ea4c9bdd44aec9cfb2ce5a643fa86f67630406af
HuongLanTo <tohuonglan1705@gmail.com>
2024-04-06 20:25:51 -0500

    169. Majority Element

---
+++ java/top-interview-150/MajorityElement.java
@@ -0,0 +1,18 @@
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (result == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                result = nums[i];
+                count = 1;
+            }
+        }
+        return result;
+    }
+}
