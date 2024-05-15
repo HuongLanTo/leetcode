e6d4b1e32c7107398df4ac43b1f0876ba1fc4854
HuongLanTo <tohuonglan1705@gmail.com>
2024-05-14 23:54:01 -0500

    374. Guess Number Higher or Lower

---
+++ javascript/guess-number-higher-or-lower.js
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let left = 1;
+    let right = n;
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        let guessNum = guess(middle);
+        if (guessNum === 1) {
+            left = middle + 1;
+        } else if (guessNum === -1) {
+            right = middle - 1;
+        } else if (guessNum === 0) {
+            return middle;
+        }
+    }
+};
